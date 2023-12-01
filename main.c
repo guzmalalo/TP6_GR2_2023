2c510031faf13f488a51aa0df360536761930351
Eduardo Guzman <eduardo.guzman@innovamics.com>
2023-12-01 11:40:00 +0100

    exmeple snoopy

--- main.c
+++ main.c
@@ -1,3 +1,40 @@
+#include <stdio.h>
+
+struct s {
+    int x;
+    int y;
+    int vies;
+};
+
+typedef struct s Snoopy;
+
+void initSnoopy(Snoopy* psnoopy){
+    (*psnoopy).x = 10;
+    psnoopy->y = 5;
+    psnoopy->vies =3;
+}
+
+void tueSnoopy(Snoopy* psnoopy){
+    psnoopy->vies = 0;
+}
+
+void depSnoopy(Snoopy* psnoopy, int dx, int dy){
+    psnoopy->x += dx;
+    psnoopy->y += dy;
+}
+
 int main(){
+
+    Snoopy monSnoopy;
+    initSnoopy(&monSnoopy);
+
+    depSnoopy(&monSnoopy, 10, 10);
+
+    printf("Les coordonées de snoopy sont %d %d \n", monSnoopy.x, monSnoopy.y);
+
+    tueSnoopy(&monSnoopy);
+
+    printf("Vies snoopy %d", monSnoopy.vies);
+
     return 0;
 }
